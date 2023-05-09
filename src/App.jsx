6e32f93f2589780ae27f52fb725a9f8596cb601d
Polina Sokolova <polina2001sokolovas@gmail.com>
2023-05-09 11:52:41 +0200

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import React from "react"
+import React, { useRef } from 'react';
+import html2canvas from 'html2canvas';
 import Header from "./components/Header"
 import Meme from "./components/Meme"
-import DownloadButton from "./components/DownloadButton"
 import "./App.css"
-function App() {
 
+function App() {
+  async function handleImageDownload() {
+    const element = document.getElementById('print'),
+    canvas = await html2canvas(element),
+    data = canvas.toDataURL('image/jpg'),
+    link = document.createElement('a');
+    console.log(document.getElementById("print"))
+    link.href = data;
+    link.download = 'downloaded-image.jpg';
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <div>
       <Header />
       <Meme />
-      <DownloadButton
-      image = "src/components/Trollface.svg" />
+      <div className= "d-flex justify-content-center my-2">
+        <button type="button" className = "btn my--btn" onClick={handleImageDownload}>Download</button>
+      </div>
     </div>
   )
 }
 
 export default App
+// <a className = "btn btn-default my--btn" href= "#" download = "">Download</a>
--- src/components/DownloadButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./DownloadButton.css"
-export default function DownloadButton(props) {
-  return (
-    <div className= "d-flex justify-content-center my-2">
-      <a className = "my--btn" href= {props.image} download = "Meme.svg">Download</a>
-    </div>
-  )
-}
